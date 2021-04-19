eb7e74d1176e95b1a2dee6fa1e7731436fc03907
Tan Tu <tan.tungo@gmail.com>
2021-04-19 18:13:12 +0700

    - Correctly align action mapping - Turn off RX TX leds of the arduino

--- tmk_core/protocol/usb_hid/arduino-1.0.1/variants/leonardo/pins_arduino.h
+++ tmk_core/protocol/usb_hid/arduino-1.0.1/variants/leonardo/pins_arduino.h
@@ -27,11 +27,17 @@
 
 #include <avr/pgmspace.h>
 
-#define TX_RX_LED_INIT	DDRD |= (1<<5), DDRB |= (1<<0)
-#define TXLED0			PORTD |= (1<<5)
-#define TXLED1			PORTD &= ~(1<<5)
-#define RXLED0			PORTB |= (1<<0)
-#define RXLED1			PORTB &= ~(1<<0)
+// #define TX_RX_LED_INIT	DDRD |= (1<<5), DDRB |= (1<<0)
+// #define TXLED0			PORTD |= (1<<5)
+// #define TXLED1			PORTD &= ~(1<<5)
+// #define RXLED0			PORTB |= (1<<0)
+// #define RXLED1			PORTB &= ~(1<<0)
+
+#define TX_RX_LED_INIT
+#define TXLED0
+#define TXLED1
+#define RXLED0
+#define RXLED1
 
 static const uint8_t SDA = 2;
 static const uint8_t SCL = 3;
